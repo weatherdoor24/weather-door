c39f81367e8f770616203d39e57ddee7d977b67f
stephen kore <sangameshwar.kore@climate-connect.com>
2019-05-05 17:49:19 +0530

    task::added logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
+    <link href="{{ asset('css/style.css') }}">
     @yield("css")
     @yield("js-head")
 </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,11 +14,11 @@ Welcome
 
 @section('content')
 
-<div class="container">
+<section class="container-fluid landing-bg-img">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Welcome') }}</div>
+             {{--   <div class="card-header">{{ __('Welcome') }}</div>--}}
 
                 <div class="card-body">
                     <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum </p>
@@ -27,7 +27,7 @@ Welcome
             </div>
         </div>
     </div>
-</div>
+</section>
 
 
 
